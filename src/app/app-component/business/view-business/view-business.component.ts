f440b3a5429c13360a3898bb69720da2e3eb164c
COOMECIPAR\miguel.ojeda <miguel.ojeda@coomecipar.coop.py>
2019-04-29 15:57:05 -0400

    Adecuaciones form

--- src/app/app-component/business/view-business/view-business.component.ts
+++ src/app/app-component/business/view-business/view-business.component.ts
@@ -59,18 +59,6 @@ export class ViewBusinessComponent implements OnInit {
        this.data = res.model as Business;
        this.setCurrentLocation();
     });
-    (<any>$('.dropify') ).dropify({
-        tpl: {
-            wrap:            '<div class="dropify-wrapper"></div>',
-            loader:          '<div class="dropify-loader"></div>',
-            message:         '<div class="dropify-message"><span class="file-icon" /> <p>{{ default }}</p></div>',
-            preview:         '<div class="dropify-preview"><span class="dropify-render"></span><div class="dropify-infos"><div class="dropify-infos-inner"><p class="dropify-infos-message">{{ replace }}</p></div></div></div>',
-            filename:        '<p class="dropify-filename"><span class="file-icon"></span> <span class="dropify-filename-inner"></span></p>',
-            clearButton:     '<button type="button" class="dropify-clear">{{ remove }}</button>',
-            errorLine:       '<p class="dropify-error">{{ error }}</p>',
-            errorsContainer: '<div class="dropify-errors-container"><ul></ul></div>'
-        }
-    });
   }
 
   ngOnInit() {
